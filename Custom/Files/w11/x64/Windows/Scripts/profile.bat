c297d7379480fd2bd9394e714308b5e09ddc69a1
Azarattum <azarattum@pm.me>
2023-01-26 20:38:36 +0700

    Adjusted settings. Added icons and default pins

--- Custom/Files/w11/x64/Windows/Scripts/profile.bat
+++ Custom/Files/w11/x64/Windows/Scripts/profile.bat
@@ -16,9 +16,16 @@ if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 
 echo Setting up your profile...
 attrib +r %UserProfile%
+attrib +r "%UserProfile%/AppData"
+attrib +h "%UserProfile%/Contacts"
+attrib +h "%UserProfile%/Favorites"
+attrib +h "%UserProfile%/Links"
+attrib +h "%UserProfile%/Saved Games"
+attrib +h "%UserProfile%/Searches"
 del "%UserProfile%\Desktop\*.lnk"
 rd "%AppData%\Microsoft\Windows\Start Menu\Programs\Accessories" /S /Q
 schtasks /create /xml "C:\Windows\Keymapper\keymapperd.xml" /tn "\KeymapperD"
 schtasks /create /xml "C:\Windows\Keymapper\keymapper.xml" /tn "\Keymapper"
 schtasks /run /tn "\KeymapperD"
 schtasks /run /tn "\Keymapper"
+%SYSTEMROOT%\Resources\Themes\dark.theme
