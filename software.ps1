eeda49cab3efdf9ef6228a8c0c642955bd010ade
Azarattum <azarattum@pm.me>
2023-03-03 18:07:40 +0700

    Added a software script

---
+++ software.ps1
@@ -0,0 +1,25 @@
+winget install 7zip.7zip --scope machine
+winget install Apple.iTunes --scope machine
+winget install Brave.Brave --scope machine
+winget install Discord.Discord
+winget install Figma.Figma
+winget install Mega.MEGASync --scope machine
+winget install Microsoft.VisualStudioCode --scope machine
+winget install Obsidian.Obsidian
+winget install OBSProject.OBSStudio --scope machine
+winget install ONLYOFFICE.DesktopEditors
+winget install PrismLauncher.PrismLauncher -l "C:\Game Files\Minecraft"
+winget install ProtonTechnologies.ProtonVPN --scope machine
+winget install qBittorrent.qBittorrent --scope machine
+winget install Telegram.TelegramDesktop --scope machine
+winget install Valve.Steam --scope machine
+winget install WerWolv.ImHex
+winget install ZeroTier.ZeroTierOne --scope machine 
+
+winget install Neovim.Neovim --scope machine
+winget install Azul.ZuluFX.19.JDK
+winget install Git.Git --scope machine
+winget install Microsoft.VisualStudio.2022.BuildTools
+winget install OpenJS.NodeJS --scope machine
+winget install Python.Python.3.12 --scope machine
+winget install Rustlang.Rustup
