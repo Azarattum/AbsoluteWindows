e86dc36c17601079666741b8127fea0efd230ed3
Azarattum <azarattum@pm.me>
2023-03-16 14:48:57 +0700

    Made profile run without admin privileges

--- Custom/Files/w11/x64/Windows/Scripts/profile.bat
+++ Custom/Files/w11/x64/Windows/Scripts/profile.bat
@@ -1,19 +1,4 @@
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (GOTO askAdmin)
-goto gotAdmin
-:askAdmin
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-goto UACPrompt
-) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-exit /B
-:gotAdmin
-if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-
+@echo off
 echo Setting up your profile...
 attrib +r %UserProfile%
 attrib +r "%UserProfile%/AppData"
@@ -23,9 +8,11 @@ attrib +h "%UserProfile%/Links"
 attrib +h "%UserProfile%/Saved Games"
 attrib +h "%UserProfile%/Searches"
 del "%UserProfile%\Desktop\*.lnk"
-rd "%AppData%\Microsoft\Windows\Start Menu\Programs\Accessories" /S /Q
-schtasks /create /xml "C:\Windows\Keymapper\keymapperd.xml" /tn "\KeymapperD"
-schtasks /create /xml "C:\Windows\Keymapper\keymapper.xml" /tn "\Keymapper"
-schtasks /run /tn "\KeymapperD"
-schtasks /run /tn "\Keymapper"
-%SYSTEMROOT%\Resources\Themes\dark.theme
+
+start %SYSTEMROOT%\Resources\Themes\dark.theme
+timeout /t 1 /nobreak
+reg add "HKEY_CURRENT_USER\Control Panel\International" /v "sShortDate" /t REG_SZ /d "ddd, d MMM" /f
+taskkill /im explorer.exe /f
+start explorer.exe
+timeout /t 1 /nobreak
+taskkill /im SystemSettings.exe /f
